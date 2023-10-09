85f732e19600438e597d4085d8b97c664b4614bf
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-10-09 20:35:51 +0900

    feat: Set up shortcuts

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
 import 'package:custom_fragment_shader/home_page.dart';
+import 'package:custom_fragment_shader/intents.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,7 +14,14 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      shortcuts: _shortcuts,
       home: const HomePage(),
     );
   }
 }
+
+const _shortcuts = <ShortcutActivator, Intent>{
+  SingleActivator(LogicalKeyboardKey.arrowLeft): BackIntent(),
+  SingleActivator(LogicalKeyboardKey.arrowRight): NextIntent(),
+  SingleActivator(LogicalKeyboardKey.period): MenuIntent(),
+};
---
+++ lib/intents.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BackIntent extends Intent {
+  const BackIntent();
+}
+
+class NextIntent extends Intent {
+  const NextIntent();
+}
+
+class MenuIntent extends Intent {
+  const MenuIntent();
+}
