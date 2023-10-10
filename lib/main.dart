215315f7d0964101e52d6b6ded981f4cd8eb67c6
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-10-10 23:31:21 +0900

    improve: Use the PathUrlStrategy to handle the browser URL

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:custom_fragment_shader/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(const App());
 }
