2faabee041f77bebb369a36054d12d3f408bb97d
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-10-14 19:54:39 +0900

    improve: Set up window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
+
 import 'package:custom_fragment_shader/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isMacOS) {
+    setWindowMinSize(const Size(640, 360));
+  }
   usePathUrlStrategy();
   runApp(const App());
 }
