aea3d728be7560f7e79b4a05f7af35a3e4dd4c6e
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-10-15 19:47:44 +0900

    fix: Fix `Error: Unsupported operation: Platform._operatingSystem`

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
 
 import 'package:custom_fragment_shader/app.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:window_size/window_size.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  if (Platform.isMacOS) {
+  if (!kIsWeb && Platform.isMacOS) {
     setWindowMinSize(const Size(640, 360));
   }
   usePathUrlStrategy();
