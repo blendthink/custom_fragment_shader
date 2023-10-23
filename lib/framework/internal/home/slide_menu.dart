643e36bf8859450608086be61fd358d4a1f38997
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-10-23 19:15:46 +0900

    feat: Implement the SlidePreviews

--- lib/framework/internal/home/slide_menu.dart
+++ lib/framework/internal/home/slide_menu.dart
@@ -1,5 +1,8 @@
+import 'package:custom_fragment_shader/framework/internal/home/slide_background.dart';
 import 'package:custom_fragment_shader/framework/internal/home/slide_frame_query.dart';
 import 'package:custom_fragment_shader/framework/internal/slide_framework.dart';
+import 'package:custom_fragment_shader/framework/internal/slide_query.dart';
+import 'package:custom_fragment_shader/framework/slide_widget.dart';
 import 'package:flutter/material.dart';
 
 final class SlideMenu extends StatelessWidget {
@@ -19,7 +22,10 @@ final class SlideMenu extends StatelessWidget {
           bottom: shouldShowMenu ? 0 : -menuHeight,
           duration: const Duration(milliseconds: 200),
           curve: Curves.easeInOut,
-          child: const _Menu(),
+          child: const ColoredBox(
+            color: Colors.black54,
+            child: _SlidePreviews(),
+          ),
         );
       },
     );
@@ -34,13 +40,92 @@ final class MenuValueNotifier extends ValueNotifier<bool> {
   }
 }
 
-final class _Menu extends StatelessWidget {
-  const _Menu();
+final class _SlidePreviews extends StatelessWidget {
+  const _SlidePreviews();
 
   @override
   Widget build(BuildContext context) {
-    return const ColoredBox(
-      color: Colors.amber,
+    final slides = context.framework.slides;
+    final gap = SizedBox(width: 12 * context.frameScale);
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: slides.length + 2,
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          return gap;
+        }
+        if (index == slides.length + 1) {
+          return gap;
+        }
+        final slideIndex = index - 1;
+        final slide = slides[slideIndex];
+        return _SlidePreview(
+          index: slideIndex,
+          slide: slide,
+          onTap: () => context.framework.goToSlide(slideIndex),
+        );
+      },
+      separatorBuilder: (context, index) {
+        return gap;
+      },
+    );
+  }
+}
+
+final class _SlidePreview extends StatelessWidget {
+  const _SlidePreview({
+    required this.index,
+    required this.slide,
+    required this.onTap,
+  });
+
+  final int index;
+  final SlideStatelessWidget slide;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final data = MediaQuery.of(context);
+    final textStyle = Theme.of(context).textTheme.labelMedium;
+    final isSelected = index == context.slideNumber;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          '$index',
+          style: textStyle,
+        ),
+        SizedBox(height: 8 * context.frameScale),
+        Material(
+          color: Colors.transparent,
+          child: Ink(
+            height: context.menuHeight / 1.5,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: isSelected ? Colors.white : Colors.transparent,
+                width: 2 * context.frameScale,
+              ),
+            ),
+            child: InkWell(
+              onTap: onTap,
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: MediaQuery(
+                  data: data.copyWith(
+                    textScaleFactor: context.frameScale / 4.5,
+                  ),
+                  child: Stack(
+                    children: [
+                      const SlideBackground(),
+                      slide,
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
