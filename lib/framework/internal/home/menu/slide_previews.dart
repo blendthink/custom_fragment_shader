94ed7b346c9711f75072c19367d559cf6b63a1d2
blendthink <admin@blendthink.dev>
2023-10-24 09:01:48 +0900

    feat: Scroll to the specified preview when `slideNumber` is changed

--- lib/framework/internal/home/menu/slide_previews.dart
+++ lib/framework/internal/home/menu/slide_previews.dart
@@ -2,16 +2,41 @@ import 'package:custom_fragment_shader/framework/internal/home/menu/slide_previe
 import 'package:custom_fragment_shader/framework/internal/home/menu/slide_preview_query.dart';
 import 'package:custom_fragment_shader/framework/internal/home/slide_frame_query.dart';
 import 'package:custom_fragment_shader/framework/internal/slide_framework.dart';
+import 'package:custom_fragment_shader/framework/internal/slide_query.dart';
 import 'package:flutter/material.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 
-final class SlidePreviews extends StatelessWidget {
+final class SlidePreviews extends StatefulWidget {
   const SlidePreviews({super.key});
+
+  @override
+  State<SlidePreviews> createState() => _SlidePreviewsState();
+}
+
+class _SlidePreviewsState extends State<SlidePreviews> {
+  late final AutoScrollController _controller;
+
+  @override
+  void initState() {
+    _controller = AutoScrollController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final slides = context.framework.slides;
     final gap = SizedBox(width: 12 * context.frameScale);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _controller.scrollToIndex(context.slideNumber);
+    });
     return ListView.separated(
+      controller: _controller,
       scrollDirection: Axis.horizontal,
       itemCount: slides.length + 2,
       itemBuilder: (context, index) {
@@ -23,10 +48,15 @@ final class SlidePreviews extends StatelessWidget {
         }
         final slideIndex = index - 1;
         final slide = slides[slideIndex];
-        return SlidePreviewQuery(
-          slideIndex: slideIndex,
-          slide: slide,
-          child: const SlidePreview(),
+        return AutoScrollTag(
+          key: ValueKey(slideIndex),
+          index: slideIndex,
+          controller: _controller,
+          child: SlidePreviewQuery(
+            slideIndex: slideIndex,
+            slide: slide,
+            child: const SlidePreview(),
+          ),
         );
       },
       separatorBuilder: (context, index) {
