2ef2884d4cb43a0038edaa177be40cdac62769a6
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-10-28 13:16:15 +0900

    feat: Implement the OutroSlide

--- lib/slides/outro_slide.dart
+++ lib/slides/outro_slide.dart
@@ -1,16 +1,32 @@
+import 'package:custom_fragment_shader/components/scaler_gap.dart';
+import 'package:custom_fragment_shader/framework/internal/home/slide_frame_query.dart';
 import 'package:custom_fragment_shader/framework/slide_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 final class OutroSlide extends SlideStatelessWidget {
   const OutroSlide({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Outro Slide',
-        style: Theme.of(context).textTheme.displayMedium,
-      ),
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    final qrSize = 160 * context.frameScale;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Thank you for watching',
+          style: textTheme.displaySmall,
+        ),
+        const ScalerGap(32),
+        QrImageView(
+          data: 'https://github.com/blendthink/custom_fragment_shader',
+          size: qrSize,
+          backgroundColor: Colors.white,
+        ),
+      ],
     );
   }
 }
