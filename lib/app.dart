a418719b31de802f3dee4f5b87fb9dcedfd6e9ec
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-11-03 20:38:53 +0900

    feat: Create the Agenda02Frag01Slide, Agenda02Frag02Slide

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,8 @@ import 'package:custom_fragment_shader/slides/agenda01/agenda01_diff_between_cpu
 import 'package:custom_fragment_shader/slides/agenda01/agenda01_pipeline_highlight_slide.dart';
 import 'package:custom_fragment_shader/slides/agenda01/agenda01_pipeline_slide.dart';
 import 'package:custom_fragment_shader/slides/agenda01/agenda01_title_slide.dart';
+import 'package:custom_fragment_shader/slides/agenda02/agenda02_frag01_slide.dart';
+import 'package:custom_fragment_shader/slides/agenda02/agenda02_frag02_slide.dart';
 import 'package:custom_fragment_shader/slides/agenda02/agenda02_glsl_animation_slide.dart';
 import 'package:custom_fragment_shader/slides/agenda02/agenda02_glsl_slide.dart';
 import 'package:custom_fragment_shader/slides/agenda02/agenda02_implementation_flow_slide.dart';
@@ -41,6 +43,8 @@ final class App extends StatelessWidget {
       Agenda02ImplementationFlowStep1Slide(),
       Agenda02GlslSlide(),
       Agenda02GlslAnimationSlide(),
+      Agenda02Frag01Slide(),
+      Agenda02Frag02Slide(),
       Agenda02ImplementationFlowStep2Slide(),
       Agenda02ImplementationFlowStep3Slide(),
       Agenda03TitleSlide(),
---
+++ lib/slides/agenda02/agenda02_frag01_slide.dart
@@ -0,0 +1,55 @@
+import 'package:custom_fragment_shader/data/agenda.dart';
+import 'package:custom_fragment_shader/framework/highlight/highlight_theme.dart';
+import 'package:custom_fragment_shader/framework/highlight/highlight_view.dart';
+import 'package:custom_fragment_shader/framework/highlight/language.dart';
+import 'package:custom_fragment_shader/framework/slide_widget.dart';
+import 'package:custom_fragment_shader/templates/title_header_slide.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final class Agenda02Frag01Slide extends SlideStatelessWidget {
+  const Agenda02Frag01Slide({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const code = '''''';
+
+    final codeBlock = HighlightView(
+      code: code,
+      fileName: 'shaders/gradient.frag',
+      language: Language.glsl,
+      theme: androidStudioTheme,
+    );
+
+    final body = Row(
+      children: [
+        Expanded(
+          child: SizedBox(
+            height: double.infinity,
+            child: codeBlock,
+          ),
+        ),
+        const Expanded(child: Center()),
+      ],
+    );
+
+    return TitleHeaderSlide(
+      title: Agenda.agenda02,
+      body: body,
+    );
+  }
+
+  @override
+  String get speakerNote => '''
+ファイルの拡張子は .frag にしなければなりません。
+この拡張子以外にすると読み込まれなくなりますのでご注意ください。
+
+ファイル名をどうするかですが、今回は次のような''';
+
+  @override
+  GoRouterPageBuilder get pageBuilder => (context, state) => NoTransitionPage(
+        key: state.pageKey,
+        restorationId: state.pageKey.value,
+        child: this,
+      );
+}
