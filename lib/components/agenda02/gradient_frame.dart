2f1d7850cb958f5ac1cc0976a5525b26ccff0a59
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-11-03 20:36:58 +0900

    feat: Create the GradientFrame

---
+++ lib/components/agenda02/gradient_frame.dart
@@ -0,0 +1,48 @@
+import 'package:custom_fragment_shader/components/ticker_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_shaders/flutter_shaders.dart';
+
+final class GradientFrame extends StatelessWidget {
+  const GradientFrame({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) => ShaderBuilder(
+        assetKey: 'shaders/gradient.frag',
+        (context, shader, child) => CustomPaint(
+          size: Size(
+            constraints.maxWidth,
+            constraints.maxHeight,
+          ),
+          painter: _GradientShaderPainter(shader),
+        ),
+        child: const Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
+
+class _GradientShaderPainter extends CustomPainter {
+  const _GradientShaderPainter(this.shader);
+
+  final FragmentShader shader;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    shader.setFloat(0, size.width);
+    shader.setFloat(1, size.height);
+
+    canvas.drawRect(
+      Rect.fromLTWH(0, 0, size.width, size.height),
+      Paint()..shader = shader,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant _GradientShaderPainter oldDelegate) {
+    return oldDelegate.shader != shader;
+  }
+}
