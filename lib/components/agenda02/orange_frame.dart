cc79aef1ac9238b77c4fc46eeb50cd00ba17a90b
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2023-11-05 11:21:23 +0900

    improve: Change to use AnimatedSampler

--- lib/components/agenda02/orange_frame.dart
+++ lib/components/agenda02/orange_frame.dart
@@ -6,39 +6,19 @@ final class OrangeFrame extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) => ShaderBuilder(
-        assetKey: 'shaders/orange.frag',
-        (context, shader, child) => CustomPaint(
-          size: Size(
-            constraints.maxWidth,
-            constraints.maxHeight,
-          ),
-          painter: _GradientShaderPainter(shader),
-        ),
+    return ShaderBuilder(
+      assetKey: 'shaders/orange.frag',
+      (context, shader, child) => AnimatedSampler(
+        (image, size, canvas) {
+          canvas.drawRect(
+            Offset.zero & size,
+            Paint()..shader = shader,
+          );
+        },
         child: const Center(
           child: CircularProgressIndicator(),
         ),
       ),
     );
-  }
-}
-
-class _GradientShaderPainter extends CustomPainter {
-  const _GradientShaderPainter(this.shader);
-
-  final FragmentShader shader;
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    canvas.drawRect(
-      Rect.fromLTWH(0, 0, size.width, size.height),
-      Paint()..shader = shader,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant _GradientShaderPainter oldDelegate) {
-    return oldDelegate.shader != shader;
   }
 }
